31fa5be0690375c57710b84bd5b3c0e11a201bc3
Joseph Kim <s198022@students.aie.edu>
2019-10-14 14:45:33 -0500

    DONE

--- Math Formula/Calculator.cs
+++ Math Formula/Calculator.cs
@@ -151,17 +151,74 @@ namespace Math_Formula
 
         public void InnerTwoPoint()
         {
+            float pVarx, qVarx;
+            float pVary, qVary;
+            float pVarz, qVarz;
 
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is the Inner Point Formula");
+            Console.WriteLine("Inner(P,Q) = PxQx + PyQy + PzQz\n");
+
+            Console.WriteLine("Add an Px, Py, Pz input (Press Enter after each input)");            
+            pVarx = Convert.ToSingle(Console.ReadLine());    pVary = Convert.ToSingle(Console.ReadLine());    pVarz = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Add an Qx, Qy, Qz input (Press Enter after each input)");
+            qVarx = Convert.ToSingle(Console.ReadLine());    qVary = Convert.ToSingle(Console.ReadLine());    qVarz = Convert.ToSingle(Console.ReadLine());
+            
+            answer = (pVarx * qVarx) + (pVary + qVary) + (pVarz + qVarz);
+
+            Console.WriteLine("Inner(P,Q) = " + pVarx + "*" + qVarx + " + " + pVary + "*" + qVary + " + " + pVarz + "*" + qVarz);
+            Console.WriteLine("Answer: " + answer);
         }
 
         public void PlanePoint()
         {
+            float aVar, bVar, cVar, dVar;
+            float xVar, yVar, zVar;
 
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is the Plane Point Formula");
+            Console.WriteLine("D(P,X0) =(ax0 + by0 + cz0 + d)/(√a^2 + b^2 + c^2)");
+
+            Console.WriteLine("Add an Px, Py, Pz input (Press Enter after each input)");
+            aVar = Convert.ToSingle(Console.ReadLine());     bVar = Convert.ToSingle(Console.ReadLine());     cVar = Convert.ToSingle(Console.ReadLine());   dVar = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Add an x, y, z input (Press Enter after each input)");
+            xVar = Convert.ToSingle(Console.ReadLine());     yVar = Convert.ToSingle(Console.ReadLine());     zVar = Convert.ToSingle(Console.ReadLine());
+
+            answer = ((aVar * xVar) + (bVar * yVar) + (cVar * zVar) + dVar) / Math.Sqrt(Math.Pow(aVar, 2) + Math.Pow(bVar, 2) + Math.Pow(cVar, 2));
+
+            Console.WriteLine("D(P,X0) = (" + aVar + "*" + xVar + ") + (" + bVar + "*" + cVar + ") + (" + cVar + "*" + zVar + ") + " + dVar + " / " + aVar + "^2 + " + bVar + "^2 + " + cVar + "^2");
+            Console.WriteLine("Answer: " + answer);
         }
 
         public void CubicBeizerCurve()
         {
+            float pVar0;
+            float pVar1;
+            float pVar2;
+            float pVar3;
+            float tVar;
+   
+            Console.WriteLine(" ");
+            Console.WriteLine("Here is the Cubic Beizer Curve Formula");
+            Console.WriteLine("B(t, P0, P1, P2, P3) = (1 − t)^3P0 + 3(1 − t)^2tP1 + 3(1 − t)t^2P2 + t^3P3");
 
+            Console.WriteLine("Add an P0 input");
+            pVar0 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Add an P1 input");
+            pVar1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Add an P2 input");
+            pVar2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Add an P3 input");
+            pVar3 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Add an t input");
+            tVar = Convert.ToSingle(Console.ReadLine());
+
+            answer = ((Math.Pow((1 - tVar), 3) * pVar0) + (3 * Math.Pow((1 - tVar), 2) * tVar * pVar1) + (3 * (1 - tVar)* (tVar * tVar) * pVar2) +(Math.Pow(tVar, 3) * pVar3));
+
+            Console.WriteLine("Print Cubic Beizer Curve Formula.exe has stopped responding.");
+            Console.WriteLine("Answer: " + answer);
         }
     }
 }
